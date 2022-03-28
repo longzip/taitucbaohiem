75460bbbd41249eb28686467909571e451a8f324
Lo Van Long <longlv@v-auto.com.vn>
2022-03-28 18:36:47 +0700

    build

--- src/components/ThongTinTheBHYT.vue
+++ src/components/ThongTinTheBHYT.vue
@@ -13,9 +13,11 @@
           :color="bhyt.disabled == 1 ? 'red' : 'gray'"
       /></q-item-label>
       <q-item-label caption lines="2">
-        Mã hộ:<a target="_blank" :href="`/#/ho-gia-dinh/${bhyt.maHoGd}`">{{
-          bhyt.maHoGd
-        }}</a>
+        Mã hộ:<a
+          target="_blank"
+          :href="`https://www.buudienxatulap.ga/wordpress/wp-pwa/#/ho-gia-dinh/${bhyt.maHoGd}`"
+          >{{ bhyt.maHoGd }}</a
+        >
       </q-item-label>
       <q-item-label caption lines="2">{{ bhyt.soTheBhyt }}</q-item-label>
       <q-item-label caption lines="2">5 năm:{{ bhyt.ngay5Nam }}</q-item-label>
