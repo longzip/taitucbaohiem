d84ca7ef79cbca4ed62cb7812d75868ef29938b5
Lo Van Long <longlv@v-auto.com.vn>
2022-06-19 19:12:27 +0700

    fix

--- docs/evn.js
+++ docs/evn.js
@@ -159,6 +159,8 @@ function drawPrintEVN(trans) {
         });
     }
 
+    $("body").append('<script id="bill-template-evn-1" src="https://app.buudienxatulap.ga/temp.html" type="text/template" />');
+
     $('#bill-template-evn-1').html(`
     <div class="bill-container" style="font-size: 13px;">
     <section class="top row">
