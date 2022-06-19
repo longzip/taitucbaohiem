eda21ed444d64d09486eadf84ea2c2da3366118f
Lo Van Long <longlv@v-auto.com.vn>
2022-06-19 19:26:08 +0700

    fix

--- docs/evn.js
+++ docs/evn.js
@@ -1,29 +1,3 @@
-const txtTien = $('<input type="text" id="txt_tien_value" name="txt_tien_value" required="" data-parsley-trigger="change" data-parsley-length="[1,100]">')
-const btnSave = $('<button class="btn btn-info" type="button" onclick="saveWallet()" id="btn-save" disabled = true><i class="fa fa-save"></i> Lưu HĐ</button>');
-const btnShow = $('<button class="btn btn-primary" type="button" onclick="showWallet()" id="btn-eye"><i class="fa fa-eye"></i> Xem</button>');
-const btnLock = $('<button class="btn btn-success" type="button" onclick="unLock()" id="btn-lock"><i class="fa fa-lock"></i> Mở</button>');
-const btnTraLai = $('<button class="btn btn-secondary" type="button" onclick="traLai()" id="btn-minus">Cộng</button>');
-const btnSaoChep = $('<button class="btn btn-primary" type="button" onclick="doCopy()" id="btn-copy"><i class="fa fa-copy"></i> Copy</button>');
-const lblTongCong = $('<span id="lbl_tien" style="font-size: 28px; font-weight: 900;">0</span>');
-
-
-$("#wallet-current-balance").parent().append(btnSave);
-$("#wallet-current-balance").parent().append(btnShow);
-$("#wallet-current-balance").parent().append(btnLock);
-$("#wallet-current-balance").parent().append(txtTien);
-$("#wallet-current-balance").parent().append(btnTraLai);
-$("#wallet-current-balance").parent().append(lblTongCong);
-
-$("#btn-reset").parent().before(btnSaoChep);
-$("body").append('<script id="bill-template-evn-1" type="text/template"></script>');
-
-document.getElementById("txt_tien_value").addEventListener("keydown",
-   function(event) {
-      if (event.keyCode == 13){
-         traLai();
-      }
-   }, true);
-
 const resetCong = () => {
   localStorage.setItem("tongcong",JSON.stringify({tongCong:0, soHoaDon:[]}));
 	document.getElementById("lbl_tien").textContent = 0;
@@ -166,6 +140,32 @@ function drawPrintEVN(trans) {
 
 const khoiTao = async () => {
     await sleep(1000);
+    const txtTien = $('<input type="text" id="txt_tien_value" name="txt_tien_value" required="" data-parsley-trigger="change" data-parsley-length="[1,100]">')
+    const btnSave = $('<button class="btn btn-info" type="button" onclick="saveWallet()" id="btn-save" disabled = true><i class="fa fa-save"></i> Lưu HĐ</button>');
+    const btnShow = $('<button class="btn btn-primary" type="button" onclick="showWallet()" id="btn-eye"><i class="fa fa-eye"></i> Xem</button>');
+    const btnLock = $('<button class="btn btn-success" type="button" onclick="unLock()" id="btn-lock"><i class="fa fa-lock"></i> Mở</button>');
+    const btnTraLai = $('<button class="btn btn-secondary" type="button" onclick="traLai()" id="btn-minus">Cộng</button>');
+    const btnSaoChep = $('<button class="btn btn-primary" type="button" onclick="doCopy()" id="btn-copy"><i class="fa fa-copy"></i> Copy</button>');
+    const lblTongCong = $('<span id="lbl_tien" style="font-size: 28px; font-weight: 900;">0</span>');
+
+
+    $("#wallet-current-balance").parent().append(btnSave);
+    $("#wallet-current-balance").parent().append(btnShow);
+    $("#wallet-current-balance").parent().append(btnLock);
+    $("#wallet-current-balance").parent().append(txtTien);
+    $("#wallet-current-balance").parent().append(btnTraLai);
+    $("#wallet-current-balance").parent().append(lblTongCong);
+
+    $("#btn-reset").parent().before(btnSaoChep);
+    $("body").append('<script id="bill-template-evn-1" type="text/template"></script>');
+
+    await sleep(1000);
+    document.getElementById("txt_tien_value").addEventListener("keydown",
+    function(event) {
+        if (event.keyCode == 13){
+            traLai();
+        }
+    }, true);
     $('#bill-template-evn-1').html(`
     <div class="bill-container" style="font-size: 13px;">
     <section class="top row">
@@ -180,7 +180,7 @@ const khoiTao = async () => {
             </div>
             <div style="text-align: center;padding-top: 10px;font-size: 15px;">
                 (từ <span id="start_date">20/04/2019</span> đến <span id="end_date">20/04/2019</span>)<br>
-                Hồ Thị Thắm và Lỗ Văn Long làm ở bưu điện xã Tự Lập từ tháng 8.2021 (thay anh Lập nghỉ).
+                Hồ Thị Thắm làm ở bưu điện xã Tự Lập từ tháng 8.2021.
             </div>
         </div>
         <div class="col-3">
