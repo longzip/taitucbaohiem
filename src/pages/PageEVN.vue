453e5d9a6ea43b3e7fd68551a3e15adb71f2a9a9
Lo Van Long <longlv@v-auto.com.vn>
2022-10-06 08:12:44 +0700

    build

--- src/pages/PageEVN.vue
+++ src/pages/PageEVN.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="q-pa-md">
-    <ListHeader bgcolor="bg-orange-4">Danh sách Khách hàng EVN / Tổng: {{tongCong.toLocaleString()}}
+    <ListHeader bgcolor="bg-orange-4"
+      >Danh sách Khách hàng EVN / Tổng: {{ tongCong.toLocaleString() }}
       <q-btn
         rounded
         color="primary"
         @click="copySoDienThoaiToClipboard()"
         icon="content_copy"
       />
-      <q-btn
-        rounded
-        color="primary"
-        @click="showDialog=true"
-        icon="print"
-      />
+      <q-btn rounded color="primary" @click="showDialog = true" icon="print" />
     </ListHeader>
     <div class="q-gutter-y-md column">
       <q-input
@@ -37,11 +33,12 @@
     <q-list v-for="evn in evns" :key="evn.id">
       <q-item>
         <q-item-section>
-          <q-item-label>{{ evn.ten }}
+          <q-item-label
+            >{{ evn.ten }}
             <q-icon
-                @click="xacNhanLoaiBo(evn)"
-                :name="evn.soTien == 0 ? 'do_not_disturb_on' : 'delete_forever'"
-                :color="evn.soTien == 0 ? 'red' : 'gray'"
+              @click="xacNhanLoaiBo(evn)"
+              :name="evn.soTien == 0 ? 'do_not_disturb_on' : 'delete_forever'"
+              :color="evn.soTien == 0 ? 'red' : 'gray'"
             />
           </q-item-label>
           <q-item-label caption lines="2">{{ evn.diaChi }}</q-item-label>
@@ -51,109 +48,106 @@
 
         <q-item-section side top>
           <q-item-label caption>{{ evn.ma }}</q-item-label>
-          <q-icon
-            name="content_copy"
-            @click="copyTextToClipboard(evn.ma)"
-          /> 
-          <q-item-label caption>Số tiền: {{ parseInt(evn.soTien).toLocaleString() }}</q-item-label>
-          <q-item-label caption>{{ new Date(evn.updated_at).toLocaleString() }}</q-item-label>
-          </q-item-section
-      ></q-item>
+          <q-icon name="content_copy" @click="copyTextToClipboard(evn.ma)" />
+          <q-item-label caption
+            >Số tiền: {{ parseInt(evn.soTien).toLocaleString() }}</q-item-label
+          >
+          <q-item-label caption>{{
+            new Date(evn.updated_at).toLocaleString()
+          }}</q-item-label>
+        </q-item-section></q-item
+      >
       <q-separator spaced inset />
     </q-list>
 
     <q-dialog v-model="showDialog">
       <q-card class="my-card">
         <q-card-section class="q-pt-none">
-          <div class="text-subtitle1">
-            Bảng kê các loại tiền
-          </div>
+          <div class="text-subtitle1">Bảng kê các loại tiền</div>
           <div class="text-caption text-grey">
             <div class="row">
               <div class="col">
-                <q-input
-                dense
-                v-model="t500"
-                label="T500"
-              />
+                <q-input dense v-model="t500" label="T500" />
               </div>
               <div class="col">
-              <q-input
-              dense
-              v-model="t200"
-              label="T200"
-            />
+                <q-input dense v-model="t200" label="T200" />
               </div>
-                  <div class="col">
-                  <q-input
-                  dense
-                  v-model="t100"
-                  label="T100"
-                />
+              <div class="col">
+                <q-input dense v-model="t100" label="T100" />
               </div>
             </div>
-            
+
             <div class="row">
               <div class="col">
-                <q-input
-                  dense
-                  v-model="t50"
-                  label="T50"
-                />
+                <q-input dense v-model="t50" label="T50" />
               </div>
               <div class="col">
-                <q-input
-                  dense
-                  v-model="t20"
-                  label="T20"
-                />
+                <q-input dense v-model="t20" label="T20" />
               </div>
               <div class="col">
-                <q-input
-                  dense
-                  v-model="t10"
-                  label="T10"
-                />
+                <q-input dense v-model="t10" label="T10" />
               </div>
             </div>
-            
+
             <div class="row">
               <div class="col">
-                <q-input
-                  dense
-                  v-model="t5"
-                  label="T5"
-                />
+                <q-input dense v-model="t5" label="T5" />
               </div>
               <div class="col">
-                <q-input
-                  dense
-                  v-model="t2"
-                  label="T2"
-                />
+                <q-input dense v-model="t2" label="T2" />
               </div>
               <div class="col">
-                <q-input
-                  dense
-                  v-model="t1"
-                  label="T1"
-                />
+                <q-input dense v-model="t1" label="T1" />
               </div>
             </div>
           </div>
         </q-card-section>
         <q-card-section>
-          Tổng: {{(500000*t500+200000*t200+100000*t100+50000*t50+20000*t20+10000*t10+5000*t5+2000*t2+1000*t1).toLocaleString()}}
-          <br>
-          Còn thiếu: {{(tongCong - (500000*t500+200000*t200+100000*t100+50000*t50+20000*t20+10000*t10+5000*t5+2000*t2+1000*t1)).toLocaleString()}}
+          Tổng:
+          {{
+            (
+              500000 * t500 +
+              200000 * t200 +
+              100000 * t100 +
+              50000 * t50 +
+              20000 * t20 +
+              10000 * t10 +
+              5000 * t5 +
+              2000 * t2 +
+              1000 * t1
+            ).toLocaleString()
+          }}
+          <br />
+          Còn thiếu:
+          {{
+            (
+              tongCong -
+              (500000 * t500 +
+                200000 * t200 +
+                100000 * t100 +
+                50000 * t50 +
+                20000 * t20 +
+                10000 * t10 +
+                5000 * t5 +
+                2000 * t2 +
+                1000 * t1)
+            ).toLocaleString()
+          }}
         </q-card-section>
         <q-separator />
 
         <q-card-actions align="right">
-          <q-btn v-close-popup flat color="primary" round icon="print" @click="print()" />
+          <q-btn
+            v-close-popup
+            flat
+            color="primary"
+            round
+            icon="print"
+            @click="print()"
+          />
         </q-card-actions>
       </q-card>
-        </q-dialog>
+    </q-dialog>
   </div>
 </template>
 
@@ -172,15 +166,15 @@ export default defineComponent({
       evns: [],
       tongCong: 0,
       showDialog: false,
-      t500: '',
-      t200: '',
-      t100: '',
-      t50: '',
-      t20: '',
-      t10: '',
-      t5: '',
-      t2: '',
-      t1: '',
+      t500: "",
+      t200: "",
+      t100: "",
+      t50: "",
+      t20: "",
+      t10: "",
+      t5: "",
+      t2: "",
+      t1: "",
     };
   },
   methods: {
@@ -201,28 +195,30 @@ export default defineComponent({
           this.soTienBangKhong(evn);
         });
     },
-    async soTienBangKhong(evn){
-      const {data} = await axios.put(`https://evn-buudienxatulap.herokuapp.com/api/evns/${evn.ma}/so-tien`,{soTien: 0});
+    async soTienBangKhong(evn) {
+      const { data } = await axios.put(
+        `https://evn-buudienxatulap.herokuapp.com/api/evns/${evn.ma}/so-tien`,
+        { soTien: 0 }
+      );
       this.updateEVN(data);
     },
-    updateEVN(evn){
-      let found = this.evns.find(
-        (x) => x.ma === evn.ma
-      );
+    updateEVN(evn) {
+      let found = this.evns.find((x) => x.ma === evn.ma);
       if (found) Object.assign(found, evn);
     },
-    async timKiem(searchText,homNay=false) {
-      let url = 'https://evn-buudienxatulap.herokuapp.com/api/evns?';
-      if(searchText)
-        url += `name=${searchText}`
-      if(homNay)
-        url += `homNay=${homNay}`
+    async timKiem(searchText, homNay = false) {
+      let url = "https://evn-buudienxatulap.herokuapp.com/api/evns?";
+      if (searchText) url += `name=${searchText}`;
+      if (homNay) url += `homNay=${homNay}`;
       const { data } = await axios.get(url);
       this.evns = data;
-      this.tongCong = await data.map(t=>t.soTien).reduce(
-        ( previousValue, currentValue ) => previousValue + parseInt(currentValue),
-        0
-      );
+      this.tongCong = await data
+        .map((t) => t.soTien)
+        .reduce(
+          (previousValue, currentValue) =>
+            previousValue + parseInt(currentValue),
+          0
+        );
     },
     copyTextToClipboard(text) {
       navigator.clipboard.writeText(text).then(
@@ -242,7 +238,7 @@ export default defineComponent({
     },
     copySoDienThoaiToClipboard() {
       navigator.clipboard
-        .writeText([...new Set(this.evns.map(e => e.soDienThoai))].join())
+        .writeText([...new Set(this.evns.map((e) => e.soDienThoai))].join())
         .then(
           function () {
             Notify.create({
@@ -258,30 +254,30 @@ export default defineComponent({
           }
         );
     },
-    async print(){
-      let a = document.createElement('a');
-      a.target = '_blank';
-      let lienKet = `https://cmsbudientulap.herokuapp.com/nop-bhyt/${new Date().toISOString().slice(0,10)}/pdf?`
-      if(this.tongCong) lienKet += `tienDien=${this.tongCong}`;
-      if(this.t500) lienKet += `&t500=${this.t500}`;
-      if(this.t200) lienKet += `&t200=${this.t200}`;
-      if(this.t100) lienKet += `&t100=${this.t100}`;
-      if(this.t50) lienKet += `&t50=${this.t50}`;
-      if(this.t20) lienKet += `&t20=${this.t20}`;
-      if(this.t10) lienKet += `&t10=${this.t10}`;
-      if(this.t5) lienKet += `&t5=${this.t5}`;
-      if(this.t2) lienKet += `&t2=${this.t2}`;
-      if(this.t1) lienKet += `&t1=${this.t1}`;
+    async print() {
+      let a = document.createElement("a");
+      a.target = "_blank";
+      let lienKet = `https://cms.buudienhuyenmelinh.vn/nop-bhyt/${new Date()
+        .toISOString()
+        .slice(0, 10)}/pdf?`;
+      if (this.tongCong) lienKet += `tienDien=${this.tongCong}`;
+      if (this.t500) lienKet += `&t500=${this.t500}`;
+      if (this.t200) lienKet += `&t200=${this.t200}`;
+      if (this.t100) lienKet += `&t100=${this.t100}`;
+      if (this.t50) lienKet += `&t50=${this.t50}`;
+      if (this.t20) lienKet += `&t20=${this.t20}`;
+      if (this.t10) lienKet += `&t10=${this.t10}`;
+      if (this.t5) lienKet += `&t5=${this.t5}`;
+      if (this.t2) lienKet += `&t2=${this.t2}`;
+      if (this.t1) lienKet += `&t1=${this.t1}`;
       a.href = lienKet;
       a.click();
-    }
+    },
   },
   mounted() {
     if (this.$route.query.q) {
       this.tongCong = parseInt(this.$route.query.q);
-    }
-    else
-      this.timKiem('',true);
+    } else this.timKiem("", true);
   },
 });
 </script>
--- src/pages/PageHoSoDaXuLy.vue
+++ src/pages/PageHoSoDaXuLy.vue
@@ -8,7 +8,10 @@
         @click="xemThangTruoc()"
         icon="arrow_back"
       />
-      {{ bhyts.length }} Hồ Sơ Đã Xử Lý (T-L: {{ tham.toLocaleString()}}-{{ tongTien.toLocaleString()}}đ) / Đã nộp BHYT: {{daNopBHYT.toLocaleString()}} - Đã nộp BHXH: {{daNopBHXH.toLocaleString()}}
+      {{ bhyts.length }} Hồ Sơ Đã Xử Lý (T-L: {{ tham.toLocaleString() }}-{{
+        tongTien.toLocaleString()
+      }}đ) / Đã nộp BHYT: {{ daNopBHYT.toLocaleString() }} - Đã nộp BHXH:
+      {{ daNopBHXH.toLocaleString() }}
       <q-btn rounded color="primary" @click="dongBo()" icon="sync" />
       <q-btn
         rounded
@@ -16,20 +19,9 @@
         @click="copyTextToClipboard()"
         icon="content_copy"
       />
-      <q-btn
-        rounded
-        color="primary"
-        @click="download()"
-        icon="download"
-      />
-      <q-btn
-        rounded
-        color="primary"
-        @click="showDialog=true"
-        icon="print"
-      />
-      
-      </ListHeader>
+      <q-btn rounded color="primary" @click="download()" icon="download" />
+      <q-btn rounded color="primary" @click="showDialog = true" icon="print" />
+    </ListHeader>
 
     <div class="q-gutter-y-md column">
       <q-input
@@ -56,98 +48,95 @@
       <q-separator spaced inset />
     </q-list>
 
-        <q-dialog v-model="showDialog">
+    <q-dialog v-model="showDialog">
       <q-card class="my-card">
         <q-card-section class="q-pt-none">
-          <div class="text-subtitle1">
-            Bảng kê các loại tiền
-          </div>
+          <div class="text-subtitle1">Bảng kê các loại tiền</div>
           <div class="text-caption text-grey">
             <div class="row">
               <div class="col">
-                <q-input
-                dense
-                v-model="t500"
-                label="T500"
-              />
+                <q-input dense v-model="t500" label="T500" />
               </div>
               <div class="col">
-              <q-input
-              dense
-              v-model="t200"
-              label="T200"
-            />
+                <q-input dense v-model="t200" label="T200" />
               </div>
-                  <div class="col">
-                  <q-input
-                  dense
-                  v-model="t100"
-                  label="T100"
-                />
+              <div class="col">
+                <q-input dense v-model="t100" label="T100" />
               </div>
             </div>
-            
+
             <div class="row">
               <div class="col">
-                <q-input
-                  dense
-                  v-model="t50"
-                  label="T50"
-                />
+                <q-input dense v-model="t50" label="T50" />
               </div>
               <div class="col">
-                <q-input
-                  dense
-                  v-model="t20"
-                  label="T20"
-                />
+                <q-input dense v-model="t20" label="T20" />
               </div>
               <div class="col">
-                <q-input
-                  dense
-                  v-model="t10"
-                  label="T10"
-                />
+                <q-input dense v-model="t10" label="T10" />
               </div>
             </div>
-            
+
             <div class="row">
               <div class="col">
-                <q-input
-                  dense
-                  v-model="t5"
-                  label="T5"
-                />
+                <q-input dense v-model="t5" label="T5" />
               </div>
               <div class="col">
-                <q-input
-                  dense
-                  v-model="t2"
-                  label="T2"
-                />
+                <q-input dense v-model="t2" label="T2" />
               </div>
               <div class="col">
-                <q-input
-                  dense
-                  v-model="t1"
-                  label="T1"
-                />
+                <q-input dense v-model="t1" label="T1" />
               </div>
             </div>
           </div>
         </q-card-section>
         <q-card-section>
-          Tổng: {{(500000*t500+200000*t200+100000*t100+50000*t50+20000*t20+10000*t10+5000*t5+2000*t2+1000*t1).toLocaleString()}}
-          <br>
-          Còn thiếu: {{((daNopBHYT+daNopBHXH) - (500000*t500+200000*t200+100000*t100+50000*t50+20000*t20+10000*t10+5000*t5+2000*t2+1000*t1)).toLocaleString()}}
+          Tổng:
+          {{
+            (
+              500000 * t500 +
+              200000 * t200 +
+              100000 * t100 +
+              50000 * t50 +
+              20000 * t20 +
+              10000 * t10 +
+              5000 * t5 +
+              2000 * t2 +
+              1000 * t1
+            ).toLocaleString()
+          }}
+          <br />
+          Còn thiếu:
+          {{
+            (
+              daNopBHYT +
+              daNopBHXH -
+              (500000 * t500 +
+                200000 * t200 +
+                100000 * t100 +
+                50000 * t50 +
+                20000 * t20 +
+                10000 * t10 +
+                5000 * t5 +
+                2000 * t2 +
+                1000 * t1)
+            ).toLocaleString()
+          }}
         </q-card-section>
         <q-separator />
 
         <q-card-actions align="right">
-          <q-btn v-close-popup flat color="primary" round icon="print" @click="print()" />
+          <q-btn
+            v-close-popup
+            flat
+            color="primary"
+            round
+            icon="print"
+            @click="print()"
+          />
         </q-card-actions>
       </q-card>
-        </q-dialog>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -168,15 +157,15 @@ export default {
       daNopBHXH: 0,
       ngay: 0,
       showDialog: false,
-      t500: '',
-      t200: '',
-      t100: '',
-      t50: '',
-      t20: '',
-      t10: '',
-      t5: '',
-      t2: '',
-      t1: '',
+      t500: "",
+      t200: "",
+      t100: "",
+      t50: "",
+      t20: "",
+      t10: "",
+      t5: "",
+      t2: "",
+      t1: "",
     };
   },
   computed: {
@@ -188,7 +177,7 @@ export default {
     dongBo() {
       this.daXyLy(this.bhyts);
     },
-    traCuuTheoTen(searchText){
+    traCuuTheoTen(searchText) {
       this.thangTruoc = parseInt(searchText);
       this.xemThangTruoc();
     },
@@ -210,57 +199,91 @@ export default {
           }
         );
     },
-    xemThangTruoc(){
+    xemThangTruoc() {
       this.thangTruoc = this.thangTruoc + 1;
       this.loadData();
     },
-    async loadData(){
-      await this.hoSoDaXuLy({thangTruoc: this.thangTruoc});
-      this.tham = await this.bhyts.filter(t=>t.userId === 3152 && t.trangThaiHoSo === 9 && t.maThuTuc ===1).map(t=>t.tongTien).reduce(
-        ( previousValue, currentValue ) => previousValue + currentValue,
-        0
-      );
-      this.daNopBHYT = await this.bhyts.filter(t=>t.userId === 3152 && t.trangThaiHoSo === 4 && t.maThuTuc ===1 && new Date().getDate()-this.ngay === new Date(t.ngayNopHoSo).getDate()).map(t=>t.tongTien).reduce(
-        ( previousValue, currentValue ) => previousValue + currentValue,
-        0
-      );
-      this.daNopBHXH = await this.bhyts.filter(t=>t.userId === 3152 && t.trangThaiHoSo === 4 && t.maThuTuc ===0 && new Date().getDate()-this.ngay === new Date(t.ngayNopHoSo).getDate()).map(t=>t.tongTien).reduce(
-        ( previousValue, currentValue ) => previousValue + currentValue,
-        0
-      );
-      this.tongTien = await this.bhyts.filter(t=>t.userId === 3154 && t.trangThaiHoSo === 9 && t.maThuTuc ===1).map(t=>t.tongTien).reduce(
-        ( previousValue, currentValue ) => previousValue + currentValue,
-        0
-      );
+    async loadData() {
+      await this.hoSoDaXuLy({ thangTruoc: this.thangTruoc });
+      this.tham = await this.bhyts
+        .filter(
+          (t) => t.userId === 3152 && t.trangThaiHoSo === 9 && t.maThuTuc === 1
+        )
+        .map((t) => t.tongTien)
+        .reduce(
+          (previousValue, currentValue) => previousValue + currentValue,
+          0
+        );
+      this.daNopBHYT = await this.bhyts
+        .filter(
+          (t) =>
+            t.userId === 3152 &&
+            t.trangThaiHoSo === 4 &&
+            t.maThuTuc === 1 &&
+            new Date().getDate() - this.ngay ===
+              new Date(t.ngayNopHoSo).getDate()
+        )
+        .map((t) => t.tongTien)
+        .reduce(
+          (previousValue, currentValue) => previousValue + currentValue,
+          0
+        );
+      this.daNopBHXH = await this.bhyts
+        .filter(
+          (t) =>
+            t.userId === 3152 &&
+            t.trangThaiHoSo === 4 &&
+            t.maThuTuc === 0 &&
+            new Date().getDate() - this.ngay ===
+              new Date(t.ngayNopHoSo).getDate()
+        )
+        .map((t) => t.tongTien)
+        .reduce(
+          (previousValue, currentValue) => previousValue + currentValue,
+          0
+        );
+      this.tongTien = await this.bhyts
+        .filter(
+          (t) => t.userId === 3154 && t.trangThaiHoSo === 9 && t.maThuTuc === 1
+        )
+        .map((t) => t.tongTien)
+        .reduce(
+          (previousValue, currentValue) => previousValue + currentValue,
+          0
+        );
     },
-    async download(){
-      const taiLieus = await this.XuatD03OrD05Excel(this.bhyts.filter(t=>t.userId === 3152).map(x=>x.transactionId));
-      taiLieus.forEach(taiLieu => {
-        let a = document.createElement('a');
+    async download() {
+      const taiLieus = await this.XuatD03OrD05Excel(
+        this.bhyts.filter((t) => t.userId === 3152).map((x) => x.transactionId)
+      );
+      taiLieus.forEach((taiLieu) => {
+        let a = document.createElement("a");
         a.href = `data:${taiLieu.fileType};base64,${taiLieu.base64}`;
         a.download = taiLieu.fileName;
         a.click();
       });
     },
-    async print(){
-      let a = document.createElement('a');
-      a.target = '_blank';
-      let lienKet = `https://cmsbudientulap.herokuapp.com/nop-bhyt/${new Date().toISOString().slice(0,10)}/pdf?`
-      if(this.daNopBHYT) lienKet += `tienBHYT=${this.daNopBHYT}`;
-      if(this.daNopBHXH) lienKet += `&tienBHXH=${this.daNopBHXH}`;
-      if(this.t500) lienKet += `&t500=${this.t500}`;
-      if(this.t200) lienKet += `&t200=${this.t200}`;
-      if(this.t100) lienKet += `&t100=${this.t100}`;
-      if(this.t50) lienKet += `&t50=${this.t50}`;
-      if(this.t20) lienKet += `&t20=${this.t20}`;
-      if(this.t10) lienKet += `&t10=${this.t10}`;
-      if(this.t5) lienKet += `&t5=${this.t5}`;
-      if(this.t2) lienKet += `&t2=${this.t2}`;
-      if(this.t1) lienKet += `&t1=${this.t1}`;
+    async print() {
+      let a = document.createElement("a");
+      a.target = "_blank";
+      let lienKet = `https://cms.buudienhuyenmelinh.vn/nop-bhyt/${new Date()
+        .toISOString()
+        .slice(0, 10)}/pdf?`;
+      if (this.daNopBHYT) lienKet += `tienBHYT=${this.daNopBHYT}`;
+      if (this.daNopBHXH) lienKet += `&tienBHXH=${this.daNopBHXH}`;
+      if (this.t500) lienKet += `&t500=${this.t500}`;
+      if (this.t200) lienKet += `&t200=${this.t200}`;
+      if (this.t100) lienKet += `&t100=${this.t100}`;
+      if (this.t50) lienKet += `&t50=${this.t50}`;
+      if (this.t20) lienKet += `&t20=${this.t20}`;
+      if (this.t10) lienKet += `&t10=${this.t10}`;
+      if (this.t5) lienKet += `&t5=${this.t5}`;
+      if (this.t2) lienKet += `&t2=${this.t2}`;
+      if (this.t1) lienKet += `&t1=${this.t1}`;
       // a.href = `&tienBHYT=4988520&t500=3&t200=1&t100=4&t50=46&t20=100`;
       a.href = lienKet;
       a.click();
-    }
+    },
   },
   async mounted() {
     if (this.$route.query.ngay) {
