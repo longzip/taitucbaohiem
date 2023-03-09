49dbf84f7dff9ab010ed58e84e6e9d92e1c1f4a1
Lo Van Long <longlv@v-auto.com.vn>
2023-03-09 11:43:45 +0700

    build

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,7 @@ import axios from "axios";
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: "http://192.168.1.7:2023" });
+const api = axios.create({ baseURL: "https://192.168.1.7:2024" });
 
 const apiServices = axios.create({
   baseURL: "https://ssm-api.vnpost.vn",
