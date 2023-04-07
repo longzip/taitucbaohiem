f39d3a82a1b33db0c0b3836cfbd36d31befa2e4e
Lo Van Long <info@lovanlong.ga>
2023-04-08 06:46:14 +0700

    build

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,11 +1,14 @@
 import { boot } from "quasar/wrappers";
 import axios from "axios";
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 // import { ApolloClient, ApolloLink, InMemoryCache, createHttpLink } from "@apollo/client";
-import { ApolloClient } from 'apollo-client'
-import { ApolloLink } from 'apollo-link'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache, IntrospectionFragmentMatcher  } from 'apollo-cache-inmemory'
+import { ApolloClient } from "apollo-client";
+import { ApolloLink } from "apollo-link";
+import { createHttpLink } from "apollo-link-http";
+import {
+  InMemoryCache,
+  IntrospectionFragmentMatcher,
+} from "apollo-cache-inmemory";
 
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
@@ -14,8 +17,12 @@ import { InMemoryCache, IntrospectionFragmentMatcher  } from 'apollo-cache-inmem
 // "export default () => {}" function below (which runs individually
 // for each client)
 // export const api = axios.create({ baseURL: "https://192.168.1.7:2024" });
-export const apiKHL = axios.create({ baseURL: "https://api-portalkhl.vnpost.vn" });
-export const apiStore = axios.create({ baseURL: "https://store.hotham.vn/wordpress" });
+export const apiKHL = axios.create({
+  baseURL: "https://api-portalkhl.vnpost.vn",
+});
+export const apiStore = axios.create({
+  baseURL: "https://store.hotham.vn/wordpress",
+});
 
 export const api = axios.create({
   baseURL: "https://app.hotham.vn",
@@ -36,80 +43,80 @@ export const apiServices = axios.create({
  * Middleware operation
  * If we have a session token in localStorage, add it to the GraphQL request as a Session header.
  */
-export const middleware = new ApolloLink( ( operation, forward ) => {
-	/**
-	 * If session data exist in local storage, set value as session header.
-	 */
-	// const session = ( process.browser ) ?  localStorage.getItem( "woo-session" ) : null;
-	const session = localStorage.getItem( "woo-session" );
+export const middleware = new ApolloLink((operation, forward) => {
+  /**
+   * If session data exist in local storage, set value as session header.
+   */
+  // const session = ( process.browser ) ?  localStorage.getItem( "woo-session" ) : null;
+  const session = localStorage.getItem("woo-session");
 
-	if ( session ) {
-		operation.setContext( ( { headers = {} } ) => ( {
-			headers: {
-				"woocommerce-session": `Session ${ session }`
-			}
-		} ) );
-	}
+  if (session) {
+    operation.setContext(({ headers = {} }) => ({
+      headers: {
+        "woocommerce-session": `Session ${session}`,
+      },
+    }));
+  }
 
-	return forward( operation );
-
-} );
+  return forward(operation);
+});
 
 /**
  * Afterware operation.
  *
  * This catches the incoming session token and stores it in localStorage, for future GraphQL requests.
  */
-export const afterware = new ApolloLink( ( operation, forward ) => {
+export const afterware = new ApolloLink((operation, forward) => {
+  return forward(operation).map((response) => {
+    // if ( !process.browser ) {
+    // 	return response;
+    // }
 
-	return forward( operation ).map( response => {
+    /**
+     * Check for session header and update session in local storage accordingly.
+     */
+    const context = operation.getContext();
+    const {
+      response: { headers },
+    } = context;
+    const session = headers.get("woocommerce-session");
 
-		// if ( !process.browser ) {
-		// 	return response;
-		// }
+    if (session) {
+      // Remove session data if session destroyed.
+      if ("false" === session) {
+        localStorage.removeItem("woo-session");
 
-		/**
-		 * Check for session header and update session in local storage accordingly.
-		 */
-		const context = operation.getContext();
-		const { response: { headers } }  = context;
-		const session = headers.get( "woocommerce-session" );
+        // Update session new data if changed.
+      } else if (localStorage.getItem("woo-session") !== session) {
+        localStorage.setItem("woo-session", headers.get("woocommerce-session"));
+      }
+    }
 
-		if ( session ) {
-
-			// Remove session data if session destroyed.
-			if ( "false" === session ) {
-
-				localStorage.removeItem( "woo-session" );
-
-				// Update session new data if changed.
-			} else if ( localStorage.getItem( "woo-session" ) !== session ) {
-
-				localStorage.setItem( "woo-session", headers.get( "woocommerce-session" ) );
-
-			}
-		}
-
-		return response;
-
-	} );
-} );
+    return response;
+  });
+});
 
 export const fragmentMatcher = new IntrospectionFragmentMatcher({
-	introspectionQueryResultData: {
-		__schema: {
-		types: [], // no types provided
-		},
-	},
+  introspectionQueryResultData: {
+    __schema: {
+      types: [], // no types provided
+    },
+  },
 });
+
+const pw = "rYkOy1HCCRD0JZZcrshVYaUR39QfcG15QWUC437BMM5Pk3gNLu";
 
 // Apollo GraphQL client.
 export const client = new ApolloClient({
-	link: middleware.concat( afterware.concat( createHttpLink({
-		uri: 'https://store.hotham.vn/wordpress/graphql',
-		fetch: fetch
-	}) ) ),
-	cache: new InMemoryCache({ fragmentMatcher }),
+  link: middleware.concat(
+    afterware.concat(
+      createHttpLink({
+        uri: "https://store.hotham.vn/wordpress/" + pw,
+        fetch: fetch,
+      })
+    )
+  ),
+  cache: new InMemoryCache({ fragmentMatcher }),
 });
 
 export default boot(({ app }) => {
