13f2f4adbdf804de1404838ab3f9a54bb85ede81
Lo Van Long <info@lovanlong.ga>
2023-07-04 11:19:30 +0700

    build

--- src/pages/PageProducts.vue
+++ src/pages/PageProducts.vue
@@ -1,55 +1,67 @@
 <template>
-    <div class="q-pa-md">
-        <ListHeader bgcolor="bg-orange-4">
-            Giỏ hàng {{ cart?.total }} <q-icon
-                            name="remove_shopping_cart"
-                            @click="emptyCart()"
-                        />
-        </ListHeader>
-        <q-list v-if="cart?.contents?.nodes">
-            <div v-for="cartItem in cart.contents.nodes" :key="cartItem.key">
-                <q-item>
-                    <q-item-section avatar>
-                        <q-avatar rounded>
-                            <img :src="cartItem.product.node.featuredImage.node.sourceUrl">
-                        </q-avatar>
-                    </q-item-section>
-                    <q-item-section>
-                        <q-item-label>{{cartItem.product.node.name}}</q-item-label>
-                        <q-icon
-                            name="remove_shopping_cart"
-                            @click="removeItemsFromCart({keys: cartItem.key})"
-                        />
-                    </q-item-section>
-                    <q-item-section side top>
-                        <q-item-label caption>{{cartItem.quantity}}</q-item-label>
-                        <q-item-label caption>{{cartItem.total}}</q-item-label>
-                        <q-icon
-                            name="add_shopping_cart"
-                            @click="addToCart({productId: cartItem.product.node.databaseId})"
-                        />
-                    </q-item-section>
-                </q-item>
-            </div>
-        </q-list>
-        <!-- <q-btn @click="checkout()" color="white" text-color="black" label="Thanh toán" /> -->
-        <q-input outlined bottom-slots v-model="email" label="Địa chỉ email" type="email" suffix="@gmail.com" :dense="dense">
-            <template v-slot:before>
-                <q-icon name="contact_mail" />
-            </template>
+  <div class="q-pa-md">
+    <ListHeader bgcolor="bg-orange-4">
+      Giỏ hàng {{ cart?.total }}
+      <q-icon name="remove_shopping_cart" @click="emptyCart()" />
+    </ListHeader>
+    <q-list v-if="cart?.contents?.nodes">
+      <div v-for="cartItem in cart.contents.nodes" :key="cartItem.key">
+        <q-item>
+          <q-item-section avatar>
+            <q-avatar rounded>
+              <img :src="cartItem.product.node.featuredImage.node.sourceUrl" />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ cartItem.product.node.name }}</q-item-label>
+            <q-icon
+              name="remove_shopping_cart"
+              @click="removeItemsFromCart({ keys: cartItem.key })"
+            />
+          </q-item-section>
+          <q-item-section side top>
+            <q-item-label caption>{{ cartItem.quantity }}</q-item-label>
+            <q-item-label caption>{{ cartItem.total }}</q-item-label>
+            <q-icon
+              name="add_shopping_cart"
+              @click="
+                addToCart({ productId: cartItem.product.node.databaseId })
+              "
+            />
+          </q-item-section>
+        </q-item>
+      </div>
+    </q-list>
+    <!-- <q-btn @click="checkout()" color="white" text-color="black" label="Thanh toán" /> -->
+    <q-input
+      outlined
+      bottom-slots
+      v-model="email"
+      label="Địa chỉ email"
+      type="email"
+      suffix="@gmail.com"
+      :dense="dense"
+    >
+      <template v-slot:before>
+        <q-icon name="contact_mail" />
+      </template>
 
-            <template v-slot:hint>
-                Nhập địa chỉ email xác nhận đơn hàng.
-            </template>
+      <template v-slot:hint> Nhập địa chỉ email xác nhận đơn hàng. </template>
 
-            <template v-slot:append>
-                <q-btn @click="checkout({email: `${email}@gmail.com`})" round dense flat icon="shopping_cart_checkout" />
-            </template>
-        </q-input>
-        <ListHeader bgcolor="bg-orange-4">
-            Sản phẩm {{ parseInt(tongTien).toLocaleString() }}đ
-        </ListHeader>
-        <div class="q-gutter-y-md column">
+      <template v-slot:append>
+        <q-btn
+          @click="confirmThanhToan({ email: `${email}@gmail.com` })"
+          round
+          dense
+          flat
+          icon="shopping_cart_checkout"
+        />
+      </template>
+    </q-input>
+    <ListHeader bgcolor="bg-orange-4">
+      Sản phẩm {{ parseInt(tongTien).toLocaleString() }}đ
+    </ListHeader>
+    <div class="q-gutter-y-md column">
       <q-input
         outlined
         v-model="searchText"
@@ -69,74 +81,105 @@
         </template>
       </q-input>
     </div>
-        <q-list>
-            <div v-for="product in productsByName(searchText)" :key="product.id">
-                <q-item>
-                    <q-item-section avatar>
-                        <q-avatar rounded>
-                            <img :src="product.featuredImage.node.sourceUrl">
-                        </q-avatar>
-                    </q-item-section>
-                    <q-item-section>
-                        <q-item-label>{{product.name}}</q-item-label>
-                        <q-item-label caption>Mã:{{product.sku}}</q-item-label>
-                        <q-item-label caption>Kho còn:{{product.stockQuantity}}</q-item-label>
-                    </q-item-section>
-                    <q-item-section side top>
-                        <q-item-label caption>{{product.price}}</q-item-label>
-                        <q-item-label caption>{{product.regularPrice}}</q-item-label>
-                        <q-icon
-                            name="add_shopping_cart"
-                            @click="confirmAddToCart(product.productId)"
-                        />
-                    </q-item-section>
-                </q-item>
-            </div>
-        </q-list>
-
-    </div>
+    <q-list>
+      <div v-for="product in productsByName(searchText)" :key="product.id">
+        <q-item>
+          <q-item-section avatar>
+            <q-avatar rounded>
+              <img :src="product.featuredImage.node.sourceUrl" />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ product.name }}</q-item-label>
+            <q-item-label caption>Mã:{{ product.sku }}</q-item-label>
+            <q-item-label caption
+              >Kho còn:{{ product.stockQuantity }}</q-item-label
+            >
+          </q-item-section>
+          <q-item-section side top>
+            <q-item-label caption>{{ product.price }}</q-item-label>
+            <q-item-label caption>{{ product.regularPrice }}</q-item-label>
+            <q-icon
+              name="add_shopping_cart"
+              @click="confirmAddToCart(product.productId)"
+            />
+          </q-item-section>
+        </q-item>
+      </div>
+    </q-list>
+  </div>
 </template>
 <script>
 import { mapGetters, mapActions } from "vuex";
 import ListHeader from "src/components/Tasks/Modals/Shared/ListHeader.vue";
 export default {
-    components: { ListHeader },
-    data(){
-        return{
-            email: "lovanlong",
-            searchText: ""
-        }
-    },
+  components: { ListHeader },
+  data() {
+    return {
+      email: "lovanlong",
+      searchText: "",
+    };
+  },
   methods: {
-    ...mapActions("products", ["getCart","addToCart","checkout","loginUser","getProducts","removeItemsFromCart","emptyCart"]),
-    confirmAddToCart(productId){
-
+    ...mapActions("products", [
+      "getCart",
+      "addToCart",
+      "checkout",
+      "loginUser",
+      "getProducts",
+      "removeItemsFromCart",
+      "emptyCart",
+    ]),
+    confirmAddToCart(productId) {
       this.$q
-          .dialog({
-            title: "Thêm vào giỏ hàng",
-            message: "Số lượng",
-            prompt: {
-              model: 1,
-              type: "text", // optional
-            },
-            cancel: true,
-            persistent: true,
-          })
-          .onOk((data) => {
-
-            this.addToCart({
-              productId,
-              quantity: parseInt(data)
-            });
+        .dialog({
+          title: "Thêm vào giỏ hàng",
+          message: "Số lượng",
+          prompt: {
+            model: 1,
+            type: "text", // optional
+          },
+          cancel: true,
+          persistent: true,
+        })
+        .onOk((data) => {
+          this.addToCart({
+            productId,
+            quantity: parseInt(data),
           });
-    }
+        });
+    },
+    confirmThanhToan(data) {
+      this.$q
+        .dialog({
+          title: "Confirm",
+          message: "Thanh toán đơn hàng?",
+          ok: {
+            push: true,
+          },
+          cancel: {
+            color: "negative",
+          },
+          persistent: true,
+        })
+        .onOk(() => {
+          this.checkout(data);
+        });
+    },
   },
   computed: {
-    ...mapGetters("products", ["authToken", "user", "cart","products","productsByName","tongTien"])
+    ...mapGetters("products", [
+      "authToken",
+      "user",
+      "cart",
+      "products",
+      "productsByName",
+      "tongTien",
+    ]),
   },
-  mounted(){
+  mounted() {
     this.getCart();
     this.getProducts();
-  }
-}
+  },
+};
 </script>
--- src/store/module-products/actions.js
+++ src/store/module-products/actions.js
@@ -7,184 +7,182 @@ import CHECKOUT_MUTATION from "src/mutations/checkout";
 import PRODUCTS_QUERY from "src/queries/products";
 import CLEAR_CART_MUTATION from "src/mutations/clear-cart";
 import EMPTY_CART_MUTATION from "src/mutations/empty-cart";
-export async function loginUser ({commit}, {username,password}) {
-    Loading.show({
-      spinner: QSpinnerIos,
-      spinnerSize: "100px",
-    });
-    const {data} = await client.mutate({
-      mutation: LOGIN_USER,
-      variables: {
-        input: {
-          username,
-          password
-        }
-      }
-    });
-    Loading.hide();
-    commit("setAuthToken", data.login)
+export async function loginUser({ commit }, { username, password }) {
+  Loading.show({
+    spinner: QSpinnerIos,
+    spinnerSize: "100px",
+  });
+  const { data } = await client.mutate({
+    mutation: LOGIN_USER,
+    variables: {
+      input: {
+        username,
+        password,
+      },
+    },
+  });
+  Loading.hide();
+  commit("setAuthToken", data.login);
 }
 
-export async function getCart ({commit}) {
-      Loading.show({
-        spinner: QSpinnerIos,
-        spinnerSize: "100px",
-      });
-      try {
-        const {data} = await client.query({
-          query: GET_CART_QUERY,
-        });
-        commit("setCart", data)
-      } catch (error) {
-        Notify.create({
-          type: "negative",
-          message: `Đã xảy ra lỗi!`,
-        });
-      }
-      Loading.hide();
-}
-export async function addToCart ({commit},{productId,quantity=1}) {
-      Loading.show({
-        spinner: QSpinnerIos,
-        spinnerSize: "100px",
-      });
-      try {
-        const {data} = await client.mutate({
-          mutation: ADD_TO_CART,
-          variables: {
-            input: {
-              productId: parseInt(productId),
-              quantity
-            }
-          }
-        });
-        Loading.hide();
-        commit("setCart", data.addToCart)
-        Notify.create({
-          type: "positive",
-          message: `Đã thêm vào giỏ hàng!`,
-        });
-      } catch (error) {
-        Notify.create({
-          type: "negative",
-          message: `Không thể thêm sản phẩm hết hàng!`,
-        });
-      }
-
-}
-export async function removeItemsFromCart ({commit},{keys}) {
-      Loading.show({
-        spinner: QSpinnerIos,
-        spinnerSize: "100px",
-      });
-      try {
-        const {data} = await client.mutate({
-          mutation: CLEAR_CART_MUTATION,
-          variables: {
-            input: {
-              keys
-            }
-          }
-        });
-        Loading.hide();
-        commit("setCart", data.removeItemsFromCart)
-        Notify.create({
-          type: "positive",
-          message: `Đã xóa sản phẩm giỏ hàng!`,
-        });
-      } catch (error) {
-        Notify.create({
-          type: "negative",
-          message: `Không thể xóa sản phẩm giỏ hàng!`,
-        });
-      }
-
-}
-export async function emptyCart ({commit}) {
-      Loading.show({
-        spinner: QSpinnerIos,
-        spinnerSize: "100px",
-      });
-      try {
-        const {data} = await client.mutate({
-          mutation: EMPTY_CART_MUTATION,
-          variables: {
-            input: {
-
-            }
-          }
-        });
-        Loading.hide();
-        commit("setCart", data.emptyCart)
-        Notify.create({
-          type: "positive",
-          message: `Đã xóa sản phẩm giỏ hàng!`,
-        });
-      } catch (error) {
-        Notify.create({
-          type: "negative",
-          message: `Không thể xóa sản phẩm giỏ hàng!`,
-        });
-      }
-
-}
-
-export async function checkout ({commit,state,dispatch},{email}) {
-      Loading.show({
-        spinner: QSpinnerIos,
-        spinnerSize: "100px",
-      });
-      try {
-        const {data} = await client.mutate({
-          mutation: CHECKOUT_MUTATION,
-          variables: {
-            input: {
-              paymentMethod: "cod",
-              billing: {
-              //   address1: "tự lập, mê linh",
-                email,
-              //   phone: "0374638603",
-              //   lastName: "Long",
-              //   firstName: "Lỗ ",
-              //   country: "VN",
-              //   city: "Hà Nội",
-              //   state: "HN"
-              }
-            }
-        }});
-        dispatch("getCart");
-        const {checkout: {redirect}} = data;
-        const a = document.createElement("a");
-        a.target = "_blank";
-        a.href = redirect;
-        a.click();
-      } catch (error) {
-        Notify.create({
-          type: "negative",
-          message: `Không thể tạo đơn hàng hàng!`,
-        });
-      }
-      Loading.hide();
-}
-
-export async function getProducts({commit}){
+export async function getCart({ commit }) {
   Loading.show({
     spinner: QSpinnerIos,
     spinnerSize: "100px",
   });
   try {
-    const {data} = await client.query({
+    const { data } = await client.query({
+      query: GET_CART_QUERY,
+    });
+    commit("setCart", data);
+  } catch (error) {
+    Notify.create({
+      type: "negative",
+      message: `Đã xảy ra lỗi!`,
+    });
+  }
+  Loading.hide();
+}
+export async function addToCart({ commit }, { productId, quantity = 1 }) {
+  Loading.show({
+    spinner: QSpinnerIos,
+    spinnerSize: "100px",
+  });
+  try {
+    const { data } = await client.mutate({
+      mutation: ADD_TO_CART,
+      variables: {
+        input: {
+          productId: parseInt(productId),
+          quantity,
+        },
+      },
+    });
+    Loading.hide();
+    commit("setCart", data.addToCart);
+    Notify.create({
+      type: "positive",
+      message: `Đã thêm vào giỏ hàng!`,
+    });
+  } catch (error) {
+    Notify.create({
+      type: "negative",
+      message: `Không thể thêm sản phẩm hết hàng!`,
+    });
+  }
+}
+export async function removeItemsFromCart({ commit }, { keys }) {
+  Loading.show({
+    spinner: QSpinnerIos,
+    spinnerSize: "100px",
+  });
+  try {
+    const { data } = await client.mutate({
+      mutation: CLEAR_CART_MUTATION,
+      variables: {
+        input: {
+          keys,
+        },
+      },
+    });
+    Loading.hide();
+    commit("setCart", data.removeItemsFromCart);
+    Notify.create({
+      type: "positive",
+      message: `Đã xóa sản phẩm giỏ hàng!`,
+    });
+  } catch (error) {
+    Notify.create({
+      type: "negative",
+      message: `Không thể xóa sản phẩm giỏ hàng!`,
+    });
+  }
+}
+export async function emptyCart({ commit }) {
+  Loading.show({
+    spinner: QSpinnerIos,
+    spinnerSize: "100px",
+  });
+  try {
+    const { data } = await client.mutate({
+      mutation: EMPTY_CART_MUTATION,
+      variables: {
+        input: {},
+      },
+    });
+    Loading.hide();
+    commit("setCart", data.emptyCart);
+    Notify.create({
+      type: "positive",
+      message: `Đã xóa sản phẩm giỏ hàng!`,
+    });
+  } catch (error) {
+    Notify.create({
+      type: "negative",
+      message: `Không thể xóa sản phẩm giỏ hàng!`,
+    });
+  }
+}
+
+export async function checkout({ commit, state, dispatch }, { email }) {
+  Loading.show({
+    spinner: QSpinnerIos,
+    spinnerSize: "100px",
+  });
+  try {
+    const { data } = await client.mutate({
+      mutation: CHECKOUT_MUTATION,
+      variables: {
+        input: {
+          paymentMethod: "cod",
+          billing: {
+            //   address1: "tự lập, mê linh",
+            email,
+            //   phone: "0374638603",
+            //   lastName: "Long",
+            //   firstName: "Lỗ ",
+            //   country: "VN",
+            //   city: "Hà Nội",
+            //   state: "HN"
+          },
+        },
+      },
+    });
+    dispatch("getCart");
+    const {
+      checkout: { order },
+    } = data;
+    const a = document.createElement("a");
+    a.target = "_blank";
+    a.href = `https://store.hotham.vn/wordpress/wp-admin/admin-ajax.php?action=generate_wpo_wcpdf&document_type=invoice&order_ids=${order.orderNumber}&access_key=6a47fe246d`;
+    a.click();
+  } catch (error) {
+    Notify.create({
+      type: "negative",
+      message: `Không thể tạo đơn hàng hàng!`,
+    });
+  }
+  Loading.hide();
+}
+
+export async function getProducts({ commit }) {
+  Loading.show({
+    spinner: QSpinnerIos,
+    spinnerSize: "100px",
+  });
+  try {
+    const { data } = await client.query({
       query: PRODUCTS_QUERY,
       variables: {
-          first: 500,
-          where: {
-            stockStatus: "IN_STOCK",
-            supportedTypesOnly: true,
-            categoryNotIn: ["dGVybTo2MQ==","dGVybTo2NQ==",""]
-        }
-      }
+        first: 500,
+        where: {
+          stockStatus: "IN_STOCK",
+          supportedTypesOnly: true,
+          categoryNotIn: ["dGVybTo2MQ==", "dGVybTo2NQ==", ""],
+        },
+      },
     });
-    commit("setProducts", data)
+    commit("setProducts", data);
   } catch (error) {
     Notify.create({
       type: "negative",
@@ -192,5 +190,4 @@ export async function getProducts({commit}){
     });
   }
   Loading.hide();
-
 }
