0747d52bde74ae260ad2bb7db8176605842cf554
Long Web Studio <lovanlong@amycos.vn>
2024-09-26 02:11:11 +0700

    build

--- src/pages/PageTraCuuBHYT.vue
+++ src/pages/PageTraCuuBHYT.vue
@@ -1049,7 +1049,7 @@ export default {
         .post("/api/update-bhyt-data", {
           api_key: this.userDetails.isLogin, // Truyền API key từ biến
           mang_luoi_id: this.userDetails.quaTrinhCongTac.mangLuoiId, // Truyền mạng lưới ID từ biến
-          user_name: this.userDetails.userName,
+          user_name: this.userDetails.id,
           force: false,
         })
         .then((response) => {
